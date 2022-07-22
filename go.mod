fe77f51288a90a9796f683ea5105a79fbecf2d32
michaelgeorgisva <michael.georgisva@student.president.ac.id>
2022-07-22 20:58:39 +0700

    update modules v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/michaelgeorgisva/say-hello-mod
+module github.com/michaelgeorgisva/say-hello-mod/v2
 
 go 1.18
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package hello
 
-func SayHello() string {
-	return "Hello"
+func SayHello(name string) string {
+	return "Hello" + name
 }
